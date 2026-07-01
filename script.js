30d3e42d370d7a96312c1ecd269daf23438a3f9c
withamannnn <amanpersonal26@gmail.com>
2026-07-01 20:25:45 +0530

    Added Comments

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
+// Accessing elements
 const button = document.querySelector(".ripple-btn");
 const circle = document.querySelector(".bg-circle");
 
+// Applying Eventlistner
 button.addEventListener("mouseenter", (e) => {
+  // Calculating Cursor Position inside button
   const rect = button.getBoundingClientRect();
-
   const x = e.clientX - rect.left;
-  console.log(x);
-
   const y = e.clientY - rect.top;
 
+  // Adjusting Circle according to Cursor position
   circle.style.left = `${x}px`;
   circle.style.top = `${y}px`;
 });
